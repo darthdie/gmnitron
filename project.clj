5017c63697656ae070133a917d42ce6ba17d3adf
Briar Bowser <briarbear7@gmail.com>
2018-03-19 23:00:45 -0500

    Added clj-dependency.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "GNU General Public License v3.0"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-discord "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot gmnitron.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
