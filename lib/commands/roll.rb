9f0c61fc602509a5e080921b9f86754d66e7c82c
Briar Bowser <briarbear7@gmail.com>
2022-09-12 01:00:51 -0500

    Re-arranged and cleaned code. Add mid and max commands.

--- lib/commands/roll.rb
+++ lib/commands/roll.rb
@@ -3,6 +3,47 @@
 require 'active_support'
 
 module Commands
+  # class DicePool
+  #   def DicePool.fromOptions(options)
+  #     dice = (1..3).map { |i| options["die_#{i}"] }
+  #     modifier = options['modifier']
+
+  #     if modifier.present?
+  #       modifier = [modifier[0], modifier[1..].to_i]
+  #     end
+
+  #     DicePool.new(dice, modifier)
+  #   end
+
+  #   def initialize(dice, modifier)
+  #     @dice = dice
+  #     @modifier = modifier
+
+  #     rolls = dice.map do |raw_die|
+  #       normalized_die = normalize_die(raw_die)
+  #       { 
+  #         die: normalized_die,
+  #         value: roll_die(normalized_die)
+  #       }
+  #     end.sort_by { |roll| roll[:value] }
+  #   end
+
+  #   def rolls
+  #     @rolls ||= dice
+  #       .map { |raw_die| die.scan(/\d+/).first.to_i }
+  #       .map do |size|
+  #         { 
+  #           die: size,
+  #           value: roll_die(size)
+  #         }
+  #       end.sort_by { |roll| roll[:value] }
+  #   end
+
+  #   def format(effect_die)
+    
+  #   end
+  # end
+
   class Roll
     def Roll.register(bot)
       @instance ||= Roll.new(bot)
@@ -17,8 +58,52 @@ module Commands
           sub.string('modifier', 'The modifier to apply to the roll, e.g. +2')
         end
 
+        cmd.subcommand(:mid, 'Rolls a dice pool and highlights the mid die.') do |sub|
+          sub.string('die_1', 'The first die to roll, e.g. d4', required: true)
+          sub.string('die_2', 'The second die to roll, e.g. d6', required: true)
+          sub.string('die_3', 'The third die to roll, e.g. d8', required: true)
+          sub.string('modifier', 'The modifier to apply to the roll, e.g. +2')
+        end
+
+        cmd.subcommand(:max, 'Rolls a dice pool and highlights the max die.') do |sub|
+          sub.string('die_1', 'The first die to roll, e.g. d4', required: true)
+          sub.string('die_2', 'The second die to roll, e.g. d6', required: true)
+          sub.string('die_3', 'The third die to roll, e.g. d8', required: true)
+          sub.string('modifier', 'The modifier to apply to the roll, e.g. +2')
+        end
+
         bot.application_command(:roll).subcommand(:min, &method(:roll_min_command))
+        bot.application_command(:roll).subcommand(:mid, &method(:roll_mid_command))
+        bot.application_command(:roll).subcommand(:max, &method(:roll_max_command))
       end
+    end
+
+    def roll_min_command(event)
+      dice_pool = parse_dice_and_modifier(event.options)
+      rolls = roll_dice_pool(dice_pool, :min)
+
+      event.respond(content: format_dice_pool(rolls))
+    end
+
+    def roll_mid_command(event)
+      dice_pool = parse_dice_and_modifier(event.options)
+      rolls = roll_dice_pool(dice_pool, :mid)
+
+      event.respond(content: format_dice_pool(rolls))
+    end
+
+    def roll_max_command(event)
+      dice_pool = parse_dice_and_modifier(event.options)
+      rolls = roll_dice_pool(dice_pool, :max)
+
+      event.respond(content: format_dice_pool(rolls))
+    end
+
+    def parse_dice_and_modifier(options)
+      {
+        dice: (1..3).map { |i| options["die_#{i}"] },
+        modifier: parse_modifier(options['modifier'])
+      }
     end
 
     def parse_modifier(modifier)
@@ -29,18 +114,31 @@ module Commands
       [modifier[0], modifier[1..].to_i]
     end
 
-    def parse_dice_and_modifier(options)
+    def roll_dice_pool(dice_pool, effect_die)
+      rolls = dice_pool[:dice].map do |raw_die|
+        size = die_size(raw_die)
+        { 
+          size: size,
+          value: roll_die(size)
+        }
+      end.sort_by { |roll| roll[:value] }
+
+      rolled_effect_die = effect_die_from_pool(rolls, effect_die)
+
       {
-        dice: (1..3).map { |i| options["die_#{i}"] },
-        modifier: parse_modifier(options['modifier'])
+        rolls: rolls,
+        effect: rolled_effect_die,
+        total: sum_roll(rolled_effect_die, dice_pool[:modifier]),
+        modifiers: dice_pool[:modifier]
       }
     end
 
-    def normalize_die(die)
-      # TODO: do we need the d other than for display?
-      # The answer is no, so uh...remove it and re-add in display
-      size = die.scan(/\d+/).first
-      "d#{size}"
+    def die_size(die)
+      die.scan(/\d+/).first.to_i
+    end
+    
+    def roll_die(die_size)
+      rand(1..die_size)
     end
 
     def effect_die_from_pool(pool, effect_die)
@@ -54,26 +152,14 @@ module Commands
       end
     end
 
-    def roll_dice_pool(dice_pool, effect_die)
-      rolls = dice_pool[:dice].map do |raw_die|
-        normalized_die = normalize_die(raw_die)
-        { 
-          die: normalized_die,
-          value: roll_die(normalized_die)
-        }
-      end.sort_by { |roll| roll[:value] }
-      effect_die = effect_die_from_pool(rolls, effect_die)
+    def sum_roll(effect_die, modifier)
+      return effect_die[:value] unless modifier.present?
 
-      {
-        rolls: rolls,
-        effect: effect_die,
-        total: sum_roll(effect_die, dice_pool[:modifier]),
-        modifiers: dice_pool[:modifier]
-      }
+      effect_die[:value].send(modifier.first, modifier.last)
     end
 
     def format_dice_pool(pool)
-      rolls = pool[:rolls].map { |roll| "*#{roll[:die]}:* **#{roll[:value]}**" }.join(' ')
+      rolls = pool[:rolls].map { |roll| "*d#{roll[:size]}:* **#{roll[:value]}**" }.join(' ')
 
       if pool[:modifiers].empty?
         return "Rolled **#{pool[:total]}** (#{rolls})"
@@ -82,23 +168,6 @@ module Commands
       modifier_expression = pool[:modifiers].join(' ') #pool[:modifiers].map { |modifier| modifier.join(' ') }
 
       "Rolled **#{pool[:total]}** = #{pool[:effect][:value]} #{modifier_expression} (#{rolls})"
-    end
-
-    def sum_roll(effect_die, modifier)
-      return effect_die[:value] unless modifier.present?
-
-      effect_die[:value].send(modifier.first, modifier.last)
-    end
-
-    def roll_die(die)
-      rand(1..die[1..].to_i)
-    end
-
-    def roll_min_command(event)
-      dice_pool = parse_dice_and_modifier(event.options)
-      rolls = roll_dice_pool(dice_pool, :min)
-
-      event.respond(content: format_dice_pool(rolls))
     end
   end
 end
