998e2605d75611ca0cc8f96c399b2914fc17b8a3
Briar Bowser <briarbear7@gmail.com>
2022-09-23 09:12:13 -0500

    Cleanup.

--- lib/core.rb
+++ lib/core.rb
@@ -3,15 +3,6 @@
 require "discordrb"
 require "byebug"
 require 'mongoid'
-# require_relative "commands/fun"
-# require_relative "commands/roll"
-# require_relative "models/dice_pool"
-# require_relative "models/die"
-# require_relative "models/modifier"
-# require_relative "models/dice_pool_roll_formatter"
-# require_relative "models/overcome_formatter"
-# require_relative "models/mod_formatter"
-# require_relative "models/minion_roll_formatter"
 require_relative "enumerable.rb"
 
 Dir["./lib/models/*.rb"].sort.each { |file| require file }
